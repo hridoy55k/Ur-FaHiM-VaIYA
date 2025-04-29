f3fac3de6a37a7260e7794b407fe9b9e77e3c7e9
fahim55m <fahimvaiya125@gmail.com>
2025-04-29 18:13:49 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -1,31 +1,23 @@
 module.exports = {
-config: {
-name: "prefix",
-version: "1.0",
-author: "Tokodori_Frtiz",//remodified by cliff
-countDown: 5,
-role: 0,
-shortDescription: "no prefix",
-longDescription: "no prefix",
-category: "auto 🪐",
-},
+  config: {
+    name: "prefix",
+    version: "1.0",
+    author: "Tokodori_Frtiz", // remodified by cliff
+    countDown: 5,
+    role: 0,
+    shortDescription: "no prefix",
+    longDescription: "no prefix",
+    category: "auto 🪐",
+  },
 
-onStart: async function(){}, 
-onChat: async function({ event, message, getLang }) {
-if (event.body && event.body.toLowerCase() === "prefix") {
-return message.reply({
-body: `
-‣ 𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱:/ 
-‣𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱:/ 
+  onStart: async function () {},
 
-‣ 𝐀𝐝𝐦𝐢𝐧 
-‣ 𝐓 𝐀 𝐍 𝐉 𝐈 𝐋
+  onChat: async function ({ event, message }) {
+    if (event.body && event.body.toLowerCase() === "prefix") {
+      try {
+        return message.reply({
+          body: `
+‣ 𝐆𝐥𝐨𝐛𝐚𝐥 𝐏𝐫𝐞𝐟𝐢𝐱: -
+‣ 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 𝐏𝐫𝐞𝐟𝐢𝐱: mim
 
-‣ 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 ⓕ
-‣ Ťåñjïł Hāšāń Æßp 
-\n\n\n  `,
-attachment: await global.utils.getStreamFromURL("https://i.imgur.com/WxtBNOq.jpeg")
-});
-}
-}
-}
+‣ 𝐀𝐝𝐦𝐢𝐧:
